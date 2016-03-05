e30386eede88dd091106e3bda01f42a4a5368e63
Vitor Navarro <vitor.navarro87@gmail.com>
2016-03-05 17:40:17 -0300

    Initial Commit

---
+++ SwiftTODOs/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftTODOs
+//
+//  Created by Vitor Navarro on 3/5/16.
+//  Copyright © 2016 Vitor Navarro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
