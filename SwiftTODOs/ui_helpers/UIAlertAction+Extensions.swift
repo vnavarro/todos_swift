3a9ee32ea4c0ceee3d268b9dc5d228e68455f315
Vitor Navarro <vitor.navarro87@gmail.com>
2017-12-27 16:25:52 -0200

    Fixed icon height breaking constraints layout Removed height and width constraint and used aspect ration 1:1

---
+++ SwiftTODOs/ui_helpers/UIAlertAction+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIAlertAction+Extensions.swift
+//  SwiftTODOs
+//
+//  Created by Vitor Navarro on 2017-12-20.
+//  Copyright © 2017 Vitor Navarro. All rights reserved.
+//
+
+import UIKit
+
+class UIAlertAction_Extensions: UIAlertAction {
+
+}
